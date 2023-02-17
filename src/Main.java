49ef3a5e9997a56776e1cf7b5a6adc1cc795fca5
Enrique Pinzon <brayan99q@gmail.com>
2023-02-16 19:42:16 -0500

    finally

--- src/Main.java
+++ src/Main.java
@@ -10,21 +10,17 @@ public class Main {
 
         Cliente cliente1 = new Cliente(cedulaCliente1,"Enrique","Pinzon","3005434344","enrique@gmail.com", "Calle 1E # 16-16", "Tokyo");
         Empleado empleado1 = new Empleado(cedulaEmpleado1, "Andres","Gomez", "3153371063","andres@gmail.com","KR N 13 -105","Tunja","6 meses","O+","CONDUCTOR");
-        System.out.println("Numero cedula cliente: "+ cliente1.getCedula());
-        System.out.println("=================");
-        System.out.println("Numero cedula empleado: "+ empleado1.getCedula());
 
         Paquete paquete1 = new Paquete(idPaquete1, "MEDIANO", 5323,12550);
         Envio envio1 = new Envio(numeroGuia1, cliente1, "Bucaramanga",cliente1.getCiudad(),cliente1.getDireccionResidencia(), cliente1.getNombre(), hora ,"RECIBIDO",15550,paquete1);
-        System.out.println(envio1.getIdGuia());
-//        System.out.println("================================");
-//        System.out.println("Hola, gracias por usar Mensajeria Express!");
-//        System.out.println("La informacion del envio es: " + "numero de guia: " + envio1.getIdGuia() + " datos del cliente, nombres y apellidos: " + envio1.getNombreDestinatario() + " el paquete va dirigido a  " + envio1.getCiudadDestino() + " " + envio1.getDireccionDestino());
-//        System.out.println("Informacion adicional: " + "valor envio: " + envio1.getValorEnvio() );
-//        System.out.println("Informacion del paquete: " + "tipo de paquete " + paquete1.getTipoPaquete() + " valor declarado: " + paquete1.getValorDeclarado() + " peso del paquete: " + paquete1.getPeso());
-//        System.out.println("Esta informacion sera enviada al correo registrado: " + cliente1.getEmail());
-//        System.out.println("Datos de la persona que entrega el paquete: " + empleado1.getNombre() + " " + empleado1.getApellidos() + " con numero celular: " + empleado1.getCelular());
-//        System.out.println("======================");
+        System.out.println("================================");
+        System.out.println("Hola, gracias por usar Mensajeria Express!");
+        System.out.println("La informacion del envio es: " + "numero de guia: " + envio1.getIdGuia() + " datos del cliente, nombres y apellidos: " + envio1.getNombreDestinatario() + " el paquete va dirigido a  " + envio1.getCiudadDestino() + " " + envio1.getDireccionDestino());
+        System.out.println("Informacion adicional: " + "valor envio: " + envio1.getValorEnvio() );
+        System.out.println("Informacion del paquete: " + "tipo de paquete " + paquete1.getTipoPaquete() + " valor declarado: " + paquete1.getValorDeclarado() + " peso del paquete: " + paquete1.getPeso());
+        System.out.println("Esta informacion sera enviada al correo registrado: " + cliente1.getEmail());
+        System.out.println("Datos de la persona que entrega el paquete: " + empleado1.getNombre() + " " + empleado1.getApellidos() + " con numero celular: " + empleado1.getCelular());
+        System.out.println("======================");
 
     }
 }
